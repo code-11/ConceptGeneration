f51bdc860e85a292d67fe8f08ced55dd338d060d
code-11 <brendan.ritter@yahoo.com>
2017-08-05 11:03:36 -0400

    Helper functions for eventual solver

--- Stories/Outbox/D&D World/puzzle_maker.py
+++ Stories/Outbox/D&D World/puzzle_maker.py
@@ -8,7 +8,10 @@ class Tile(object):
 		self.val=val
 
 	def __str__(self):
-		return str(self.val)
+		if self.val==3.5:
+			return "o"
+		else:
+			return str(self.val)
 
 class Chunk(object):
 	tiles={}
@@ -60,22 +63,24 @@ class Chunk(object):
 
 class Agent(object):
 	tile=None
-	def __init__(self,tile):
+	name=""
+	def __init__(self,name,tile):
 		self.tile=tile
+		self.name=name
 
 	def crash(self,agent):
 		return agent.tile==self.tile
 
-class Solver(object):
+class State(object):
 	def __init__(self,agents,chunk):
 		self.agents=agents
 		self.chunk=chunk
 
 	def gen_puzz():
 		chunk=Chunk.gen_puzz()
-		agent1=Agent(chunk.tiles[(1,0)])
-		agent2=Agent(chunk.tiles[(4,0)])
-		return Solver([agent1,agent2],chunk)
+		agent1=Agent("A",chunk.tiles[(1,0)])
+		agent2=Agent("B",chunk.tiles[(4,0)])
+		return State([agent1,agent2],chunk)
 
 	def is_solved(self):
 		solved=True
@@ -83,12 +88,79 @@ class Solver(object):
 			solved=solved and (agent.tile.y==self.chunk.sizeY-1)
 		return solved
 
-	def solve(self):
+	def agent_pos(self):
+		return ((agent1.tile.x,agent1.tile.y),(agent2.tile.x,agent2.tile.y))
+
+	def new_state_on_move(self,agent,move):
+		new_pos=move(agent.tile)
+		new_agent=Agent(agent.name,new_pos)
+		other_agents=self.agents.remove(agent)
+		return State([new_agent].extend(other_agents),chunk)
+
+	def new_state_on_up(self,agent):
+		return self.new_state_on_move(agent,self.chunk.up)
+
+	def new_state_on_down(self,agent):
+		return self.new_state_on_move(agent,self.chunk.down)
+
+	def new_state_on_left(self,agent):
+		return self.new_state_on_move(agent,self.chunk.left)
+
+	def new_state_on_right(self,agent):
+		return self.new_state_on_move(agent,self.chunk.right)
+
+	def new_state_on_sit(self,agent):
+		return self.new_state_on_move(agent,lambda x: x)
+
+	def agent_str_at_pos(self,tile):
 		for agent in self.agents:
+			if agent.tile==tile:
+				return agent.name+" "
+		return "  "
+
+	def __str__(self):
+		over_str=""
+		for y in reversed(range(self.chunk.sizeY)):
+			line_str=""
+			for x in range(self.chunk.sizeX):
+				tile=self.chunk.tiles[(x,y)]
+				line_str+=(str(tile)+self.agent_str_at_pos(tile))
+			over_str+=line_str+"\n"
+		return over_str
+
+class Solver(object):
+	states=[]
+	history=[]
+	def __init__(self,initial_state):
+		self.states.append(intitial_state)
+		self.history.append(initial_state)
+
+	def new_states_from_one_agent(self,cur_state,agent):
+		new_states=[]
+		new_states.append(cur_state.new_state_on_up(cur_agent))
+		new_states.append(cur_state.new_state_on_down(cur_agent))
+		new_states.append(cur_state.new_state_on_left(cur_agent))
+		new_states.append(cur_state.new_state_on_right(cur_agent))
+		new_states.append(cur_state.new_state_on_sit(cur_agent))
+		print(new_states)
+
+	# def 
+
+	# def solve(self):
+	# 	while len(self.states)>0:
+	# 		#check rules on collision and 7 here as well as end state
+	# 		seed_state=states.pop(0)
+	# 		cur_states=[seed_state]
+	# 		while len(cur_states)>0:
+	# 			cur_state=cur_states.pop(0)
+	# 			while len(cur_state.agents)>0:
+	# 				cur_agent=cur_state.agents.pop(0)
 
 
 
-print(Chunk.gen_puzz())
+
+
+print(State.gen_puzz())
 # a=Chunk(4,4)
 # print(a)
 # print(a.down(a.right(a.right(a.tiles[(1,2)]))))
